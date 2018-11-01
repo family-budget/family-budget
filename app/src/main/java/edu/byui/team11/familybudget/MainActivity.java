b2a77810379343e114c0e6b665c26237699e2c72
Elias Ramirez <ram16032@byui.edu>
2018-10-31 20:18:36 -0500

    Added another line

--- app/src/main/java/edu/byui/team11/familybudget/MainActivity.java
+++ app/src/main/java/edu/byui/team11/familybudget/MainActivity.java
@@ -4,6 +4,7 @@ package edu.byui.team11.familybudget;
 // This is for Teach 07 - Luis
 // This is for Teach 07 - Elias
 
+//test comment
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
